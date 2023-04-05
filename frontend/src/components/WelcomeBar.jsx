47d45a07fd489a78f7ac34cd124c7f3cf6e19243
Tweedle2Dum <utkarsh.work1405@gmail.com>
2023-04-06 03:32:54 +0530

    basic dashboard layout setup and work started

---
+++ frontend/src/components/WelcomeBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box,Text,Heading,Avatar } from '@chakra-ui/react'
+
+export default function WelcomeBar() {
+  return (
+    <Box display={"flex"} marginTop="10px">
+        <Box marginLeft={"20px"}>
+        <Heading>Welcome User !</Heading>
+        <Text>Some random text regarding the content</Text>
+        </Box>
+        <Avatar name="User User" src="" size={"xl"} marginLeft="auto" marginRight={"20px"} ></Avatar>
+    </Box>
+    )
+}
--- frontend/src/pages/DashboardHome.jsx
+++ frontend/src/pages/DashboardHome.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
-import { Box } from '@chakra-ui/react'
+import React from "react";
+import { Box, Grid } from "@chakra-ui/react";
+import WelcomeBar from "../components/WelcomeBar";
 export default function DashboardHome() {
   return (
-    <Box> Content</Box>
-  )
+    <Box>
+      <Grid
+      gridGap={"20px"}
+      gridTemplateColumns={"1fr 1fr 1fr"}
+      gridTemplateRows = {"8em 20vh 20vh 15em"}
+      gridTemplateAreas={" 'h h h' 'g1 g1 g2' 'g1 g1 g3' 'g4 g5 .' "}>
+        <Box gridArea={"h"}>
+          <WelcomeBar />
+        </Box>
+        <Box gridArea={"g1"} bgColor="orange">
+          {" "}
+          graph 1
+        </Box>
+        <Box gridArea={"g2"} bgColor="purple">
+          graph 2
+        </Box>
+        <Box gridArea={"g3"} bgColor="pink">
+          graph 3
+        </Box>
+        <Box gridArea={"g4"} bgColor="white">
+          graph 3
+        </Box>
+        <Box gridArea={"g5"} bgColor="green">
+          graph 3
+        </Box>
+      </Grid>
+    </Box>
+  );
 }
-
