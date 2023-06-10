b24330dc1add01cf4006ccba76e60162d16088cc
Tweedle2Dum <utkarsh.work1405@gmail.com>
2023-06-11 00:59:20 +0530

    added skeletal componenet for projects

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import CreateProject from '../components/Dashboard/Projects/CreateProject'
+import { Box } from '@chakra-ui/react'
 export default function Projects() {
   return (
     <>
-        Projects page 
+        <Box display={"flex"}>
+          <Box marginLeft={"auto"}><CreateProject/></Box>
+        
+        </Box>
     </>
   )
 }
