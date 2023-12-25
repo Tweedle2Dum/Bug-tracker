8a06111260d13f0a054c089e91510b6025ed31f9
Tweedle2Dum <utkarsh.work1405@gmail.com>
2023-12-25 13:07:53 +0530

    initial commit

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,30 @@
-package main 
+package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"strings"
 )
 
-func main () {
+func main() {
 
-	app:= fiber.New()
+	app := fiber.New()
+	app.Use(logger.New(logger.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return strings.HasPrefix(c.Path(), "/")
+		},
+	}))
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "*",
+	}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+
+		return c.SendString("Hello world")
+	})
+
+	app.Listen(":3001")
 }
